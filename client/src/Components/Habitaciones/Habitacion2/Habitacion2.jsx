be688f118fe1444f701522e1fdb17673b5c98def
francollovera <francollovera@gmail.com>
2023-05-30 16:29:59 -0300

    link funcionales

---
+++ client/src/Components/Habitaciones/Habitacion2/Habitacion2.jsx
@@ -0,0 +1,187 @@
+import React, { useState } from 'react';
+import NavBar from '../../NavBar/NavBar';
+import FooterBar from '../../FooterBar/FooterBar';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from './Suite Canelo/1.jpg'
+import image2 from './Suite Canelo/2.jpg'
+import image3 from './Suite Canelo/3.jpg'
+import image4 from './Suite Canelo/4.jpg'
+import image5 from './Suite Canelo/5.jpg'
+import image6 from './Suite Canelo/6.jpg'
+import style from './Habitacion2.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faMoneyBill, faPersonBooth  } from '@fortawesome/free-solid-svg-icons';
+
+const Habitacion1 = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div className={style.containertotal}>
+      <NavBar></NavBar>
+      <section >
+      <h1 className={style.titulo}>Suite Canell</h1>
+        <div className={style.texto}>
+        
+       
+          <p>
+          Con una cómoda tinaja privada a los pies del río,<br></br>  vistas hacia el bosque,y en medio de un entorno<br></br>  natural único, suite Canell destaca por por su acogedor<br></br>  entorno, 2 ambientes, luminosidad y decoración con <br></br>cálidos materiales y elementos sureños.<br></br> Queremos que tengas la más cómoda y acogedora estadía.
+          </p>
+          </div>
+
+
+          
+          <div className={style.container}>
+      <h2 className={style.title}>Características</h2>
+      
+  
+    <div className={style.centerleft}>
+    <div>
+    <div className={style.image1}><FontAwesomeIcon icon={faBed}  /></div>
+      
+     
+      
+    </div>
+      <p>Esta habitación posee cama súper King,<br></br> cama Nido y/o camas adicionales. Cuenta con<br></br> terraza y salida directa al río Liucura, vistas al<br></br> bosque, caja de seguridad, aire acondicionado y<br></br> calefacción termal, Tv cable,<br></br> batas, desayuno buffet y teléfono.</p>
+    </div>
+  
+  
+    <div className={style.centerrigth}>
+    <div className={style.image2}>< FontAwesomeIcon icon={faPersonBooth} /></div>
+      <p>Este tipo de habitación se encuentra disponible <br></br>para un máximo de 4 personas.</p>
+    </div>
+  
+  
+    <div className={style.center}>
+    <div className={style.image3}>< FontAwesomeIcon icon={faMoneyBill} /></div>
+     
+      <p>Revisa el detalle de los valores de esta habitación en <br></br>nuestra política de precios y estadía en nuestro<br></br> Centro de Ayuda.</p>
+    </div>
+    </div>
+        
+        </section>
+      <div 
+      className="container w-100">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image1}
+              alt="First slide"
+              width={"100%"}
+              height={"750px"}
+            />
+            
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image2}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image3}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image4}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image5}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image6}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      
+        <div >
+            <h2 className={style.caracteristicastitulo}>Incluye</h2>
+            </div>
+        <section className={style.caracteristicas}>
+          
+          
+          <ul>
+            <li>Vistas panorámicas</li>
+            <li>Aire Acondicionado</li>
+            <li>Cama Super King</li>
+            <li>Hidromasaje</li>
+            <li>Telefono</li>
+            <li>Salida al rio</li>
+            <li>Terraza</li>
+            <li>Caja de Seguridad</li>
+            <li>TV por cable</li>
+          </ul>
+        </section>
+
+
+        <div className={style.titulodisponibilidad}><h2>Disponibilidad</h2></div>
+        <section className={style.disponibilidad}>
+          
+          <p>Verifica la disponibilidad y realiza tu reserva en línea:</p>
+        
+        </section>
+        <div className={style.containerlink}><a className={style.linka} href="#">Ver disponibilidad</a></div>
+        
+        <FooterBar className={style.footer} />
+      
+      
+    </div>
+  );
+};
+
+export default Habitacion1;
---
+++ client/src/Components/Habitaciones/Habitacion4/Habitacion4.jsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import NavBar from '../../NavBar/NavBar';
+import FooterBar from '../../FooterBar/FooterBar';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from './Villa Bosque/1.jpg'
+import image2 from './Villa Bosque/2.jpg'
+import image3 from './Villa Bosque/3.jpg'
+import image4 from './Villa Bosque/4.jpg'
+import image5 from './Villa Bosque/5.jpg'
+
+import style from './Habitacion4.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faMoneyBill, faPersonBooth  } from '@fortawesome/free-solid-svg-icons';
+
+const Habitacion1 = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div className={style.containertotal}>
+      <NavBar></NavBar>
+      <section >
+      <h1 className={style.titulo}>Suite Roma</h1>
+        <div className={style.texto}>
+        
+       
+          <p>
+            Disfruta de la máxima comodidad en nuestra Suite Roma. <br></br>La habitación
+            perfecta para relajarte y disfrutar de las <br></br>vistas panorámicas.
+          </p>
+          </div>
+
+
+          
+          <div className={style.container}>
+      <h2 className={style.title}>Características</h2>
+      
+  
+    <div className={style.centerleft}>
+    <div>
+    <div className={style.image1}><FontAwesomeIcon icon={faBed}  /></div>
+      
+     
+      
+    </div>
+      <p>Esta habitación posee cama súper King con la posibilidad de <br></br>cama adicional, tina con agua termal, caja de seguridad,<br></br> minibar, aire acondicionado y calefacción termal, TV cable, batas,<br></br> mesa de desayuno, desayuno buffet,<br></br> teléfono, vistas al bosque y río.</p>
+    </div>
+  
+  
+    <div className={style.centerrigth}>
+    <div className={style.image2}>< FontAwesomeIcon icon={faPersonBooth} /></div>
+      <p>Este tipo de habitación se encuentra disponible para <br></br>un máximo de dos personas..</p>
+    </div>
+  
+  
+    <div className={style.center}>
+    <div className={style.image3}>< FontAwesomeIcon icon={faMoneyBill} /></div>
+     
+      <p>Revisa el detalle de los valores de esta habitación en <br></br>nuestra política de precios y estadía en nuestro<br></br> Centro de Ayuda.</p>
+    </div>
+    </div>
+        
+        </section>
+      <div 
+      className="container w-100">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image1}
+              alt="First slide"
+              width={"100%"}
+              height={"750px"}
+            />
+            
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image2}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image3}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image4}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image5}
+              alt="First slide"
+              width="100%"
+              height="750px"
+            />
+            <Carousel.Caption>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          
+        </Carousel>
+      </div>
+      
+        <div >
+            <h2 className={style.caracteristicastitulo}>Incluye</h2>
+            </div>
+        <section className={style.caracteristicas}>
+          
+          
+          <ul>
+            <li>Vistas panorámicas</li>
+            <li>Cama king-size</li>
+            <li>Hidromasaje</li>
+            <li>Chimenea</li>
+            <li>TV de pantalla plana</li>
+          </ul>
+        </section>
+
+
+        <div className={style.titulodisponibilidad}><h2>Disponibilidad</h2></div>
+        <section className={style.disponibilidad}>
+          
+          <p>Verifica la disponibilidad y realiza tu reserva en línea:</p>
+        
+        </section>
+        <div className={style.containerlink}><a className={style.linka} href="#">Ver disponibilidad</a></div>
+        
+        <FooterBar className={style.footer} />
+      
+      
+    </div>
+  );
+};
+
+export default Habitacion1;
