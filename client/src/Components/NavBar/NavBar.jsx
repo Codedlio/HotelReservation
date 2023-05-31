8c080cbd31578b01786ba1c6a0b1aea93eb8709c
francollovera <francollovera@gmail.com>
2023-05-31 15:44:11 -0300

    Subido

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -11,12 +11,12 @@ function NavBar() {
       <img className={style.imagen} src={imagen} alt="" />
       
       <a className={style.link}><Link to='/'><p>Inicio</p></Link></a>
-      <a className={style.link}><p>Novedades</p></a>
+      <a className={style.link}><p>Iniciar sesion</p></a>
       <a className={style.link}><p>Contacto</p></a>
       <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faInstagram} /></a></p>
         <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faFacebook} /></a></p>
         <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faTwitter} /></a></p>
-      <button className={style.button} ><Link  className={style.button1} to='/reserva'>RESERVAR AHORA</Link></button>
+      <Link  className={style.button} to='/reserva'>RESERVAR AHORA</Link>
     </div>
   );
 }
--- client/src/Components/Reserva/Reserva.jsx
+++ client/src/Components/Reserva/Reserva.jsx
@@ -1,37 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Reserva.module.css';
 import { Link } from 'react-router-dom';
 
+
 function Reserva() {
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleAdultsChange = (e) => {
+    setAdults(parseInt(e.target.value));
+  };
+
+  const handleChildrenChange = (e) => {
+    setChildren(parseInt(e.target.value));
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  if (!isOpen) {
+    return null; // Retorna null para ocultar el componente cuando no está abierto
+  }
+
   return (
+    
     <div className={style.contenedor}>
+     
       <div className={style.tamano}>
-      <h3 className={style.title}>Reserva tu estadía</h3>
-      <form>
-        <div className={style.formGroup}>
-          <label htmlFor="check-in" className={style.label}>
-            Fecha de entrada:
-          </label>
-          <input type="date" id="check-in" className={style.input} required />
-        </div>
-        <div className={style.formGroup}>
-          <label htmlFor="check-out" className={style.label}>
-            Fecha de salida:
-          </label>
-          <input type="date" id="check-out" className={style.input} required />
-        </div>
-        <div className={style.formGroup}>
-          <label htmlFor="guests" className={style.label}>
-            Cantidad de huéspedes:
-          </label>
-          <input type="number" id="guests" className={style.input} min="1" required />
-        </div>
-        <button className={style.button}>
-          
+      <Link className={style.linkContainer} to="/">
+            <button className={style.closeButton} onClick={handleClose}>
+              X
+            </button>
+          </Link>
+        
+        <h3 className={style.title}>Reserva tu estadía</h3>
+        <form>
+          <div className={style.formGroup}>
+            <label htmlFor="check-in" className={style.label}>
+              Fecha de entrada:
+            </label>
+            <input type="date" id="check-in" className={style.input} required />
+          </div>
+          <div className={style.formGroup}>
+            <label htmlFor="check-out" className={style.label}>
+              Fecha de salida:
+            </label>
+            <input type="date" id="check-out" className={style.input} required />
+          </div>
+          <div className={style.formGroup}>
+            <label htmlFor="adults" className={style.label}>
+              Adultos:
+            </label>
+            <input
+              type="number"
+              id="adults"
+              className={style.input}
+              min="1"
+              value={adults}
+              onChange={handleAdultsChange}
+              required
+            />
+          </div>
+          <div className={style.formGroup}>
+            <label htmlFor="children" className={style.label}>
+              Niños:
+            </label>
+            <input
+              type="number"
+              id="children"
+              className={style.input}
+              min="0"
+              value={children}
+              onChange={handleChildrenChange}
+              required
+            />
+          </div>
+          <button className={style.button}>
             Reservar ahora
-          
-        </button>
-      </form>
+          </button>
+        </form>
       </div>
     </div>
   );
