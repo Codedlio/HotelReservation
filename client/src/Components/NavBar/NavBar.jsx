af4fe75e8ed446de4bae43281d2d0ad9171078a6
francollovera <francollovera@gmail.com>
2023-06-01 01:04:05 -0300

    linkinstagram

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -5,6 +5,11 @@ import { faInstagram, faFacebook, faTwitter } from "@fortawesome/free-brands-svg
 import imagen from './logo hotel2.png';
 import { Link } from "react-router-dom";
 
+
+  const handleInstagramClick = () => {
+    window.open('https://www.instagram.com/hoteleternotermal/', '_blank');
+  }
+
 function NavBar() {
   return (
     <div className={style.contenedor}>
@@ -13,7 +18,7 @@ function NavBar() {
       <Link className={style.link} to='/'>Inicio</Link>
       <Link className={style.link} to='/'>Iniciar sesion</Link>
       <Link className={style.link} to='/contacto'>Contacto</Link>
-      <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faInstagram} /></a></p>
+      <p><a className={style.icon} href="#" onClick={handleInstagramClick}><FontAwesomeIcon icon={faInstagram} /></a></p>
         <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faFacebook} /></a></p>
         <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faTwitter} /></a></p>
       <Link  className={style.button} to='/reserva'>RESERVAR AHORA</Link>
