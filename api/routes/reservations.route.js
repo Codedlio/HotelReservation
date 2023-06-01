be220c65309db673675bcc94d0e2cbf5c33587d0
Andariel <workoanda@gmail.com>
2023-06-01 18:14:31 -0300

    Error fix de modelo Usuario y middlewares de app.js

--- api/routes/reservations.route.js
+++ api/routes/reservations.route.js
@@ -1,12 +1,13 @@
 
-const {getReservations,postReservation,putReservation} = require('../controllers/controllerReservations');
+const {getReservations,postReservation,putReservation, deleteReservation} = require('../controllers/controllerReservations');
 const express =require('express')
 const routerReservation=express.Router()
 
 routerReservation
     .get('/', getReservations)
     .post('/', postReservation)
-    .put('/:id', putReservation);
+    .put('/:id', putReservation)
+    .delete('/:id', deleteReservation);
 
 module.exports=routerReservation;
  
