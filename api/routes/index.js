4892135e83a7ecbe8e30547c9b3796a119149980
Andariel <workoanda@gmail.com>
2023-06-01 18:14:31 -0300

    Error fix de modelo Usuario y middlewares de app.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 
 const hotelsRoute = require('./hotels.route')
 const reservationsRoute = require('./reservations.route')
-
-
+const habitacionesRoute = require('./habitaciones.route')
+const serviciosRoute = require('./servicios.route')
 
 const router = Router();
 router.use('/hotel', hotelsRoute)
 router.use('/reservation', reservationsRoute)
+router.use('/habitacion', habitacionesRoute)
+router.use('/servicio', serviciosRoute)
 
 
 module.exports=router;
