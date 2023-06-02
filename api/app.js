09bb6da460de480a6f4728b3e027e94a191c3d8a
Andariel <workoanda@gmail.com>
2023-06-02 18:28:06 -0300

    Fixed modelo Reservacion. Finalizadas rutas de Reservacion

--- api/app.js
+++ api/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const cors=require('cors');
 const routes = require('./routes/index.js');
-const morgan = require('morgan');
 const server = express();
 
 server.use(express.json());
-server.use(morgan("dev"));
 server.use(cors())
 server.use('/', routes);
 
