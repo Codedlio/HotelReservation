e76782a6b4c43dffa832dab1de673248fa3e3ec6
francollovera <francollovera@gmail.com>
2023-06-02 20:45:21 -0300

    filtrosfuncionando

--- client/src/Components/Habitaciones/Habitaciones.jsx
+++ client/src/Components/Habitaciones/Habitaciones.jsx
@@ -3,43 +3,42 @@ import { Link } from "react-router-dom";
 import style from "./Habitaciones.module.css";
 import imagen from './habitacion.jpg';
 import { connect } from 'react-redux';
-import { setOrderByName } from '../redux/action';
+import { setOrderByName, setOrderByCapacity } from '../redux/action';
 
 const mapStateToProps = (state) => {
   return {
-    orderByName: state.orderByName
+    orderByName: state.orderByName,
+    orderByCapacity: state.orderByCapacity,
+    habitacionesData: state.habitacionesData
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    setOrderByName: (orderType) => dispatch(setOrderByName(orderType))
+    setOrderByName: (orderType) => dispatch(setOrderByName(orderType)),
+    setOrderByCapacity: (orderType) => dispatch(setOrderByCapacity(orderType))
   };
 };
 
 class Habitaciones extends React.Component {
   render() {
-    const habitaciones = [
-      { nombre: 'Suite Roca' },
-      { nombre: 'Suite Canelo' },
-      { nombre: 'Suite Liucura' },
-      { nombre: 'Villa Bosque' },
-      { nombre: 'Villa Rio' },
-      { nombre: 'Villa Arce' },
-      { nombre: 'Villa Tilo' },
-      { nombre: 'Villa Cedra' },
-      { nombre: 'Villa Madrid' }
-    ];
-
-    // Filtrar y ordenar las habitaciones según el estado de ordenamiento
-    let habitacionesFiltradas = habitaciones;
+    const habitacionesFiltradas = this.props.habitacionesData.slice();
+    
+    // Filtrar y ordenar las habitaciones según el estado de ordenamiento por nombre
     if (this.props.orderByName === 'asc') {
-      habitacionesFiltradas = habitacionesFiltradas.sort((a, b) => a.nombre.localeCompare(b.nombre));
+      habitacionesFiltradas.sort((a, b) => a.nombre.localeCompare(b.nombre));
     } else if (this.props.orderByName === 'desc') {
-      habitacionesFiltradas = habitacionesFiltradas.sort((a, b) => b.nombre.localeCompare(a.nombre));
+      habitacionesFiltradas.sort((a, b) => b.nombre.localeCompare(a.nombre));
     }
 
-    // Renderizar las habitaciones filtradas
+    // Filtrar y ordenar las habitaciones según el estado de ordenamiento por capacidad
+    if (this.props.orderByCapacity === 'asc') {
+      habitacionesFiltradas.sort((a, b) => a.capacidad - b.capacidad);
+    } else if (this.props.orderByCapacity === 'desc') {
+      habitacionesFiltradas.sort((a, b) => b.capacidad - a.capacidad);
+    }
+
+    // Renderizar los enlaces de las habitaciones filtradas
     const habitacionLinks = habitacionesFiltradas.map((habitacion, index) => (
       <Link to={`/habitacion${index + 1}`} className={style.link} key={index}>
         {habitacion.nombre}
@@ -48,16 +47,16 @@ class Habitaciones extends React.Component {
 
     return (
       <div className={style.section}>
-        <select className={style.linksordenamiento} onChange={(e) => this.props.setOrderByName(e.target.value)}>
+        <select className={style.linksordenamiento} value={this.props.orderByName} onChange={(e) => this.props.setOrderByName(e.target.value)}>
           <option value="">Ordenar por Nombre</option>
           <option value="asc">A-Z</option>
           <option value="desc">Z-A</option>
         </select>
 
-        <select className={style.linksordenamiento}>
-          <option value="">Order Capacidad</option>
-          <option value="asc">Mayor</option>
-          <option value="desc">Menor</option>
+        <select className={style.linksordenamiento} value={this.props.orderByCapacity} onChange={(e) => this.props.setOrderByCapacity(e.target.value)}>
+          <option value="">Ordenar por Capacidad</option>
+          <option value="asc">Menor a Mayor</option>
+          <option value="desc">Mayor a Menor</option>
         </select>
 
         <div className={style.content}>
--- client/src/Components/redux/reducers.js
+++ client/src/Components/redux/reducers.js
@@ -1,35 +1,76 @@
 // reducer.js
-import { SET_ORDER_BY_NAME } from "./action";
+import { SET_ORDER_BY_NAME, SET_ORDER_BY_CAPACITY } from "./action";
+
 const initialState = {
-    orderByName: '', // Estado inicial del ordenamiento por nombre
-    habitaciones: [] // Array de habitaciones
-  };
-  
-  const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case SET_ORDER_BY_NAME:
-        let sortedArr;
-        if (action.payload === 'asc') {
-          sortedArr = state.habitaciones.slice().sort((a, b) => a.nombre.localeCompare(b.nombre));
-        } else if (action.payload === 'desc') {
-          sortedArr = state.habitaciones.slice().sort((a, b) => b.nombre.localeCompare(a.nombre));
-        } else {
-          sortedArr = state.habitaciones;
-        }
-        return {
-          ...state,
-          orderByName: action.payload,
-          habitaciones: sortedArr
-        };
-      case 'SET_HABITACIONES':
-        return {
-          ...state,
-          habitaciones: action.payload
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default reducer;
-  
+  orderByName: '',
+  orderByCapacity: '',
+  habitaciones: [
+    { nombre: 'Suite Roca'},
+    { nombre: 'Suite Canelo' },
+    { nombre: 'Suite Liucura'},
+    { nombre: 'Villa Bosque' },
+    { nombre: 'Villa Rio'},
+    { nombre: 'Villa Arce' },
+    { nombre: 'Villa Tilo' },
+    { nombre: 'Villa Cedra' },
+    { nombre: 'Villa Madrid' }
+  ],
+  habitacionesData: [
+    { nombre: 'Suite Roca', capacidad: 2 },
+    { nombre: 'Suite Canelo', capacidad: 4 },
+    { nombre: 'Suite Liucura', capacidad: 2 },
+    { nombre: 'Villa Bosque', capacidad: 7 },
+    { nombre: 'Villa Rio', capacidad: 7 },
+    { nombre: 'Villa Arce', capacidad: 6 },
+    { nombre: 'Villa Tilo', capacidad: 6 },
+    { nombre: 'Villa Cedra', capacidad: 6 },
+    { nombre: 'Villa Madrid', capacidad: 7 }
+  ]
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ORDER_BY_NAME:
+      let sortedArrByName;
+      if (action.payload === 'asc') {
+        sortedArrByName = state.habitaciones
+          .slice()
+          .sort((a, b) => a.nombre.localeCompare(b.nombre));
+      } else if (action.payload === 'desc') {
+        sortedArrByName = state.habitaciones
+          .slice()
+          .sort((a, b) => b.nombre.localeCompare(a.nombre));
+      } else {
+        sortedArrByName = state.habitaciones;
+      }
+      return {
+        ...state,
+        orderByName: action.payload,
+        habitaciones: sortedArrByName
+      };
+
+    case SET_ORDER_BY_CAPACITY:
+      let sortedArrByCapacity;
+      if (action.payload === 'asc') {
+        sortedArrByCapacity = state.habitacionesData
+          .slice()
+          .sort((a, b) => a.capacidad - b.capacidad);
+      } else if (action.payload === 'desc') {
+        sortedArrByCapacity = state.habitacionesData
+          .slice()
+          .sort((a, b) => b.capacidad - a.capacidad);
+      } else {
+        sortedArrByCapacity = state.habitacionesData;
+      }
+      return {
+        ...state,
+        orderByCapacity: action.payload,
+        habitacionesData: sortedArrByCapacity
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
