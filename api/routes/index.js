b0da5e72d197581b6532f2b97e464f0b9c954b9b
Andariel <workoanda@gmail.com>
2023-06-02 00:30:03 -0300

    Agregadas rutas de Paquete, Habitacion y Servicio

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,12 +4,13 @@ const hotelsRoute = require('./hotels.route')
 const reservationsRoute = require('./reservations.route')
 const habitacionesRoute = require('./habitaciones.route')
 const serviciosRoute = require('./servicios.route')
+const paquetesRoute = require('./paquetes.route')
 
 const router = Router();
 router.use('/hotel', hotelsRoute)
 router.use('/reservation', reservationsRoute)
 router.use('/habitacion', habitacionesRoute)
 router.use('/servicio', serviciosRoute)
-
+router.use('/paquete', paquetesRoute)
 
 module.exports=router;
