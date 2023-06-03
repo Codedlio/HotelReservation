576a5fed4b5506031f24f21a16d3b8fe6bebaf7a
francollovera <francollovera@gmail.com>
2023-06-03 18:31:51 -0300

    cambios

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -6,21 +6,7 @@ import { faInstagram, faFacebook, faTwitter } from "@fortawesome/free-brands-svg
 import imagen from './logo hotel.png';
 import { Link } from "react-router-dom";
 
-// function NavBar() {
-//   return (
-//     <div className={style.contenedor}>
-//       <img className={style.imagen} src={imagen} alt="" />
-      
-//       <a className={style.link}><Link to='/'><p>Inicio</p></Link></a>
-//       <a className={style.link}><p>Iniciar sesion</p></a>
-//       <a className={style.link}><p>Contacto</p></a>
-//       <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faInstagram} /></a></p>
-//         <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faFacebook} /></a></p>
-//         <p><a className={style.icon} href="#"><FontAwesomeIcon icon={faTwitter} /></a></p>
-//       <Link  className={style.button} to='/reserva'>RESERVAR AHORA</Link>
-//     </div>
-//   );
-// }
+
 
 
 function NavBar() {
