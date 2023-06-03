6f5f37b1a240529e7144a8edaa798013feb5e333
francollovera <francollovera@gmail.com>
2023-06-03 18:31:51 -0300

    cambios

--- client/src/Components/redux/action.js
+++ client/src/Components/redux/action.js
@@ -1,6 +1,7 @@
 // action.js
 export const SET_ORDER_BY_NAME = "SET_ORDER_BY_NAME";
 export const SET_ORDER_BY_CAPACITY = "SET_ORDER_BY_CAPACITY";
+export const SET_ORDER_BY_PRICE = "SET_ORDER_BY_PRICE";
 
 export const setOrderByName = (orderType) => {
   return {
@@ -12,6 +13,12 @@ export const setOrderByName = (orderType) => {
 export const setOrderByCapacity = (orderType) => {
   return {
     type: SET_ORDER_BY_CAPACITY,
+    payload: orderType
+  };
+};
+export const setOrderByPrice = (orderType) => {
+  return {
+    type: SET_ORDER_BY_PRICE,
     payload: orderType
   };
 };
