6feed6d387b17704adeaed7ce970a40379cd7e7c
francollovera <francollovera@gmail.com>
2023-06-02 21:23:20 -0300

    regristro

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -2,19 +2,17 @@ import React, { useState } from 'react';
 import style from './Login.module.css';
 import { Link } from 'react-router-dom';
 
+
 function Reserva() {
   const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
   const [correo, setCorreo] = useState('');
-  const [contrasena, setContrasena] = useState('');
+  const [contraseña, setContraseña] = useState('');
   const [telefono, setTelefono] = useState('');
   const [isOpen, setIsOpen] = useState(true);
+  const [usuarioCreado, setUsuarioCreado] = useState(false); // Estado para controlar si el usuario se ha creado exitosamente
+
   const handleNombreChange = (e) => {
     setNombre(e.target.value);
-  };
-
-  const handleApellidoChange = (e) => {
-    setApellido(e.target.value);
   };
 
   const handleCorreoChange = (e) => {
@@ -22,12 +20,13 @@ function Reserva() {
   };
 
   const handleContrasenaChange = (e) => {
-    setContrasena(e.target.value);
+    setContraseña(e.target.value);
   };
 
   const handleTelefonoChange = (e) => {
     setTelefono(e.target.value);
   };
+
   const handleClose = () => {
     setIsOpen(false);
   };
@@ -37,26 +36,51 @@ function Reserva() {
     // Aquí puedes realizar acciones con los datos enviados, como enviarlos a un servidor
     console.log('Datos enviados:', {
       nombre,
-      
       correo,
-      contrasena,
+      contraseña,
       telefono
     });
+
+    // Envío de datos al servidor
+    fetch('http://localhost:3001/auth/registro', {
+      method: 'POST',
+      body: JSON.stringify({
+        nombre,
+        correo,
+        contraseña,
+        telefono
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        // Manejar la respuesta del servidor
+        if (response.ok) {
+          setUsuarioCreado(true); // Actualizar el estado para indicar que el usuario se ha creado exitosamente
+        }
+      })
+      .catch(error => {
+        // Manejar errores
+      });
   };
 
   return (
     <div className={style.contenedor}>
-         <Link className={style.linkContainer} to="/">
-          <button className={style.botoncerrar} onClick={handleClose}>
-            X
-          </button>
-        </Link>
+      <Link className={style.linkContainer} to="/">
+        <button className={style.botoncerrar} onClick={handleClose}>
+          X
+        </button>
+      </Link>
       <div className={style.tamano}>
-        <h3 className={style.title}>Reserva tu estadía</h3>
+        <h3 className={style.title}>Crear Nueva Cuenta</h3>
+        {usuarioCreado && (
+          <div className={style.mensajeExito}>Usuario creado con éxito</div>
+        )}
         <form onSubmit={handleSubmit}>
           <div className={style.formGroup}>
             <label htmlFor="nombre" className={style.label}>
-              Nombre y Apellido:
+              Nombre:
             </label>
             <input
               type="text"
@@ -67,7 +91,6 @@ function Reserva() {
               required
             />
           </div>
-          
           <div className={style.formGroup}>
             <label htmlFor="correo" className={style.label}>
               Correo Electrónico:
@@ -82,14 +105,14 @@ function Reserva() {
             />
           </div>
           <div className={style.formGroup}>
-            <label htmlFor="contrasena" className={style.label}>
+            <label htmlFor="contraseña" className={style.label}>
               Contraseña:
             </label>
             <input
               type="password"
-              id="contrasena"
+              id="contraseña"
               className={style.input}
-              value={contrasena}
+              value={contraseña}
               onChange={handleContrasenaChange}
               required
             />
@@ -107,7 +130,9 @@ function Reserva() {
               required
             />
           </div>
-          <button type="submit" className={style.button}>Enviar</button>
+          <button type="submit" className={style.button}>
+            Enviar
+          </button>
         </form>
       </div>
     </div>
