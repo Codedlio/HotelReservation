eb407fde0a86af43a86f1d4d63a2b2db4604d9d7
Andariel <workoanda@gmail.com>
2023-06-14 20:49:04 -0300

    Filtro por precio. Filtros combinados

--- client/src/Components/Habitaciones/Habitaciones.jsx
+++ client/src/Components/Habitaciones/Habitaciones.jsx
@@ -5,18 +5,19 @@ import style from "./Habitaciones.module.css";
 import imagen from './habitacion.jpg';
 
 import { connect } from 'react-redux';
-import { setOrderByName, setOrderByCapacity } from '../redux/action';
+import { setOrderByName, setOrderByCapacity, getHabitaciones } from '../redux/action';
 
 const mapStateToProps = (state) => {
   return {
     orderByName: state.orderByName,
     orderByCapacity: state.orderByCapacity,
-    habitacionesData: state.habitacionesData
+    habitaciones: state.gethabitaciones
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    getHabitaciones: () => dispatch(getHabitaciones()),
     setOrderByName: (orderType) => dispatch(setOrderByName(orderType)),
     setOrderByCapacity: (orderType) => dispatch(setOrderByCapacity(orderType))
   };
@@ -26,7 +27,9 @@ class Habitaciones extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchQuery: ''
+      searchQuery: '',
+      minPrice: 0,
+      maxPrice: 0
     };
   }
 
@@ -34,32 +37,53 @@ class Habitaciones extends React.Component {
     this.setState({ searchQuery: event.target.value });
   };
 
-  render() {
-    let habitacionesFiltradas = this.props.habitacionesData.slice();
+  handleMinPriceChange = (event) => {
+    this.setState({ minPrice: parseInt(event.target.value)});
+  };
 
-    // Filtrar y ordenar las habitaciones según el estado de ordenamiento por nombre
+  handleMaxPriceChange = (event) => {
+    this.setState({ maxPrice: parseInt(event.target.value)});
+  };
+
+  componentDidMount() {
+    this.props.getHabitaciones();
+  };
+
+  render() {
+    let habitacionesFiltradas = this.props.habitaciones.slice();
+    
+    
     if (this.props.orderByName === 'asc') {
       habitacionesFiltradas.sort((a, b) => a.nombre.localeCompare(b.nombre));
     } else if (this.props.orderByName === 'desc') {
       habitacionesFiltradas.sort((a, b) => b.nombre.localeCompare(a.nombre));
     }
 
-    // Filtrar y ordenar las habitaciones según el estado de ordenamiento por capacidad
     if (this.props.orderByCapacity === 'asc') {
       habitacionesFiltradas.sort((a, b) => a.capacidad - b.capacidad);
     } else if (this.props.orderByCapacity === 'desc') {
       habitacionesFiltradas.sort((a, b) => b.capacidad - a.capacidad);
     }
 
-    // Filtrar habitaciones según el valor de búsqueda
     if (this.state.searchQuery) {
       habitacionesFiltradas = habitacionesFiltradas.filter((habitacion) =>
         habitacion.nombre.toLowerCase().includes(this.state.searchQuery.toLowerCase())
       );
     }
 
-    // Renderizar los enlaces de las habitaciones filtradas
-    const habitacionLinks = habitacionesFiltradas.map((habitacion, index) => (
+    if (this.state.minPrice !== 0) {
+      habitacionesFiltradas = habitacionesFiltradas.filter((habitacion) => 
+        habitacion.precio >= this.state.minPrice
+      )
+    }
+
+    if (this.state.maxPrice !== 0) {
+      habitacionesFiltradas = habitacionesFiltradas.filter((habitacion) => 
+        habitacion.precio <= this.state.maxPrice
+      )
+    }    
+
+    let habitacionLinks = habitacionesFiltradas.map((habitacion, index) => (
       <Link to={`/habitacion${index + 1}`} className={style.link} key={index}>
         {habitacion.nombre}
       </Link>
@@ -87,6 +111,22 @@ class Habitaciones extends React.Component {
           className={style.searchInput}
         />
 
+        <input 
+          type="number"
+          placeholder="Precio mínimo"
+          value={this.state.minPrice}
+          onChange={this.handleMinPriceChange}
+          className={style.searchInput}        
+        />
+
+        <input 
+          type="number"
+          placeholder="Precio máximo"
+          value={this.state.maxPrice}
+          onChange={this.handleMaxPriceChange}
+          className={style.searchInput}
+        />
+        
         <div className={style.content}>
           <img src={imagen} alt="Habitación" className={style.image} />
           
