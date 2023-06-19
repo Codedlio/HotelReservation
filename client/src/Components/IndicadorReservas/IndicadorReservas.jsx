19243a34d942e8ad67e30e19f429dd029d47124b
Codedlio <alexis.leonel_@hotmail.com>
2023-06-18 19:32:51 -0600

    indicadoresReserva

--- client/src/Components/IndicadorReservas/IndicadorReservas.jsx
+++ client/src/Components/IndicadorReservas/IndicadorReservas.jsx
@@ -2,23 +2,38 @@ import React from "react";
 import style from "./IndicadorReservas.module.css";
 import NavBar from '../NavBar/NavBar';
 import FooterBar from '../FooterBar/FooterBar'
-
-
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+//import roomData from "../../../src/reservacions.json";
 function IndicadorReservas() {
-  
+//   const servicios = data.servicios.map(servicio => servicio);
+// const paquetes = data.paquetes.map(paquete => paquete);
+
+  const roomData=[ { roomType: 'Individual', revenue: 1200 },
+  { roomType: 'Doble', revenue: 2500 },
+  { roomType: 'Suite', revenue: 4000 },]
+  //const data =roomData.map(room=>room.precio)
   return (
-    
+
     <div className={style.body}>
     <NavBar />   
     <div className={style.container}>         
-      <h2>Indicador de Reservas</h2>  
+      <h2>Indicador de Reclamos</h2>  
       <div className={style.centerleft}></div>  
-      <p></p>
+      <p></p><BarChart width={600} height={300} data={roomData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="roomType" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="revenue" fill="#8884d8" />
+    </BarChart>
     </div>
     <FooterBar/>
 </div>
-      
     
+
+    
+ 
   );
 }
 
