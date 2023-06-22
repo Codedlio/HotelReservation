c914a7f3ce14b316c7efbed3625b6e632f074798
Andariel <workoanda@gmail.com>
2023-06-22 01:14:14 -0300

    Fix controller getReservacionByUsuario

--- api/controllers/controllerReservations.js
+++ api/controllers/controllerReservations.js
@@ -105,99 +105,40 @@ const getReservacionById = async (req,res) => {
 
 const getReservacionByUsuario = async (req,res) => {
   const {usuario} = req.params;
-  try {
-        
+  try {  
     let ReservaUsu = [];
-    let reservacion = await Reservacion.find({usuario:usuario,activo:true,estado:'I'});       
+    let reservacion = await Reservacion.find({usuario:usuario,activo:true});       
     if (reservacion.length==0) {    
       return res.status(200).send(ReservaUsu)
     };
-    let ReservacionDeUsuario=reservacion.slice(-1);        
-    for (let habitacionId of ReservacionDeUsuario) {
-        
-        let hab=[];     
-        let habFin=[];     
-        for (let habId of habitacionId.habitaciones) { 
-          let habitacionXReserva = await Habitacion.findOne({_id:habId});
-          hab.push(habitacionXReserva);        
-          
-          for (let h of hab) {
-            if (h) {
-              var obj = {
-                _id: habId,
-                nombre: h.nombre,
-                capacidad: h.capacidad,
-                precio: h.precio,
-                image: h.image
-              };
-              habFin.push(obj);
-            }
-          }       
-                  
-        }
-        let serv=[];     
-        let servFin=[]; 
-        for (let servId of habitacionId.servicios) {         
-          
-          let servXReserva = await Servicio.findOne({_id:servId});
-          serv.push(servXReserva);    
-          for (let s of serv) {  
-            if(s) {
-               var obj={
-              _id:servId,
-              nombre:s.nombre,
-              descripcion: s.descripcion,
-              precio: s.precio
-            };        
-          }
-          servFin.push(obj);
-            }        
-                   
-                  
-        }
-        let paq=[];     
-        let paqFin=[]; 
-        for (let paqId of habitacionId.paquetes) { 
-          let paqXReserva = await Paquete.findOne({_id:paqId});
-          paq.push(paqXReserva);        
-          //console.log("paq");
-          //console.log(paq);
-          for (let p of paq) {    
-            if(p){
-              var obj={
-              _id:paqId,
-              nombre:p.nombre,
-              // capacidad: p.capacidad,
-              precio: p.costo,
-              image: p.image
-            };        
-          }
-          paqFin.push(obj); 
-            }       
-                   
-                  
-        }
+    let ReservacionDeUsuario=reservacion.slice(-1);  
 
-        var objh={
-          _id:habitacionId._id,
-          usuario: habitacionId.usuario,
-          fechaInicio: habitacionId.fechaInicio,
-          fechaFin:habitacionId.fechaFin,
-          image:habitacionId.image,
-          costo:habitacionId.costo,
-          Arrayhabitaciones:habFin,
-          Arraypaquete:paqFin,
-          ArrayServicio:servFin,
-          fechaReserva:habitacionId.fechaReserva,
-          nroPerson:habitacionId.nroPerson
-        };  
-    }
-    ReservaUsu.push(objh);
-    return res.status(200).json(ReservaUsu);
-} 
-catch (error) {
-    return res.status(500).send(error.message);
-}
+    let arrHabitaciones = [];
+    for (const habitacionId of ReservacionDeUsuario[0].habitaciones) {
+      let obj = await Habitacion.findById(habitacionId);
+      arrHabitaciones.push(obj)
+    };
+
+    let arrServicios = [];
+    for (const servicioId of ReservacionDeUsuario[0].servicios) {
+      let obj = await Servicio.findById(servicioId);
+      arrServicios.push(obj)
+    };
+
+    let arrPaquetes = [];
+    for (const paqueteId of ReservacionDeUsuario[0].paquetes) {
+      let obj = await Paquete.findById(paqueteId);
+      arrPaquetes.push(obj);
+    };  
+
+    ReservacionDeUsuario[0].habitaciones = arrHabitaciones;
+    ReservacionDeUsuario[0].servicios = arrServicios;
+    ReservacionDeUsuario[0].paquetes = arrPaquetes;
+    return res.status(200).json(ReservacionDeUsuario);
+  } 
+  catch (error) {
+      return res.status(500).send(error.message); 
+  }
 };
 
 const postReservacion = async (req,res) => {
